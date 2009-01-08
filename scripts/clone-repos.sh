60bb32a3f61b779328cbbb0017c6d00d603f5598
Tom Bruno <tbruno@neurostechnology.com>
2009-01-08 12:39:38 -0600

    change clone repo to reflect wallpapers name change. Fix duplication and other bad links in sources.list

--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -12,7 +12,7 @@ git clone git@$1:repo/neuroslink-meta
 git clone git@$1:repo/neuroslink-restricted-extras
 git clone git@$1:repo/neuroslink-system-tools
 git clone git@$1:repo/neuroslink-dev-tools
-git clone git@$1:repo/neuros-wallpapers
+git clone git@$1:repo/neuroslink-wallpapers
 git clone git@$1:repo/mythtv-0.21.0+fixes18722
 git clone git@$1:repo/mythtv-theme-neuroslink
 git clone git@$1:repo/firefox-default-profile
